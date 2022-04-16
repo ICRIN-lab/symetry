84030866982bae4c7dc951547940a9e5756c0ec9
mselleki <mselleki98@gmail.com>
2022-04-16 16:08:05 +0200

    final version symetry

--- symetry.py
+++ symetry.py
@@ -11,7 +11,7 @@ class Symetry(TaskTemplate):
     nb_ans = 2
     response_pad = True  # has to be set on "True" on production.
     # END OF IMPORTANT
-    trials = 30
+    trials = 20
     yes_key_name = "verte"
     yes_key_code = "6"
     no_key_name = "rouge"
