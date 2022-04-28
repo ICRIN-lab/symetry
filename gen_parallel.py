9acd81293e2d69d0bfa5d8f14ac3dbb61c5fbf21
mselleki <mselleki98@gmail.com>
2022-04-28 15:44:48 +0200

    final version Symetry

--- gen_parallel.py
+++ gen_parallel.py
@@ -1,28 +1,30 @@
 # petit code qui génère des barres parallèles
+import random
 from PIL import Image, ImageDraw, ImageFont
 from screeninfo import get_monitors
 from random import randint
-nb_img = 30
+nb_img = 103
 
 
 for i in range(nb_img):
+    print(f"### IMAGE N°{i}")
     epsilon = randint(3, 5)
-    for j in range(5):
-        img = Image.new(mode="RGB", size=(get_monitors()[0].width, get_monitors()[0].height), color=(255, 255, 255))
-        if j == 4:
-            angle = randint(0, 180)
-            foreground = Image.open("barre.png").rotate(angle, resample=3, expand=True)
-            x = randint(0, get_monitors()[0].width - 700)
-            y = randint(0, get_monitors()[0].height - 700)
-            img.paste(foreground, (x, y),
-                      foreground)
-            foreground = Image.open("barre.png").rotate(angle+epsilon, expand=True)
-            img.paste(foreground, (x + randint(150, 400), y + randint(150, 400)),
-                      foreground)
-        else:
-            foreground = Image.open("barre.png").rotate(45, expand=True)
-            x = randint(0, get_monitors()[0].width - 700)
-            y = randint(0, get_monitors()[0].height - 700)
-            img.paste(foreground, (x, y), foreground)
-            img.paste(foreground, (x + randint(150, 400), y + randint(150, 400)), foreground)
-        img.save(f"img/img_{i}_{j}.png")
+    img = Image.new(mode="RGB", size=(get_monitors()[0].width, get_monitors()[0].height), color=(255, 255, 255))
+    if i < 25:
+        angle = randint(0, 180)
+        foreground = Image.open("barre.png").rotate(angle, resample=3, expand=True)
+        x = randint(0, get_monitors()[0].width - 700)
+        y = randint(0, get_monitors()[0].height - 700)
+        img.paste(foreground, (x, y),
+                  foreground)
+        foreground = Image.open("barre.png").rotate(angle+epsilon, expand=True)
+        img.paste(foreground, (x + randint(150, 400), y + randint(150, 400)),
+                  foreground)
+
+    else:
+        foreground = Image.open("barre.png").rotate(45, expand=True)
+        x = randint(0, get_monitors()[0].width - 700)
+        y = randint(0, get_monitors()[0].height - 700)
+        img.paste(foreground, (x, y), foreground)
+        img.paste(foreground, (x + randint(150, 400), y + randint(150, 400)), foreground)
+    img.save(f"img/img_{i}.png")
--- symetry.py
+++ symetry.py
@@ -4,12 +4,13 @@ import time
 
 from psychopy import core
 from Template_Task_Psychopy.task_template import TaskTemplate
+from list_images import images
 
 
 class Symetry(TaskTemplate):
     # IMPORTANT ! To MODIFY IF NEEDED
     nb_ans = 2
-    response_pad = True  # has to be set on "True" on production.
+    response_pad = False  # has to be set on "True" on production.
     # END OF IMPORTANT
     trials = 10
     yes_key_name = "verte"
@@ -18,6 +19,7 @@ class Symetry(TaskTemplate):
     no_key_code = "0"
     quit_code = "3"
     keys = ["space", yes_key_code, no_key_code, quit_code]
+    launch_example = True
 
     next = f"Pour passer à l'instruction suivante, appuyez sur la touche {yes_key_name}"
 
@@ -28,31 +30,59 @@ class Symetry(TaskTemplate):
 
         f"Placez vos index sur les touches {no_key_name} et {yes_key_name}."]
 
-    csv_headers = ['id_candidate', 'no_trial', 'num_subtrial', 'ans_candidate',
+    csv_headers = ['id_candidate', 'no_trial', 'ans_candidate',
                    'good_ans', 'correct', 'reaction_time', 'time_stamp']
 
     def task(self, no_trial, exp_start_timestamp, trial_start_timestamp, practice=False, count_image=1):
+        self.create_visual_image(image=f'img/{images[0]}',
+                                 size=self.size(images[0])).draw()
+        self.win.flip()
+        core.wait(2)
+        self.create_visual_text("Les deux barres sont-elles parallèles ? \n\n Non / Oui").draw()
+        self.win.flip()
+        time_stamp = time.time() - exp_start_timestamp
+        resp, rt = self.get_response_with_time(self.response_pad)
+        good_ans = [self.yes_key_code if no_trial >= 25 and no_trial != 102 else self.no_key_code][0]
+        if self.response_pad:
+            self.update_csv(self.participant, no_trial, resp, good_ans, resp == good_ans,
+                            round(rt - time_stamp, 2), round(rt, 2))
+        else:
+            self.update_csv(self.participant, no_trial, resp, good_ans, resp == good_ans, round(rt, 2),
+                            round(time.time() - exp_start_timestamp, 2))
+        images.pop(0)
+        if self.launch_example:
+            return resp == good_ans
 
-        index_list = [0, 1, 2, 3, 4]
-        while True:
-            j = index_list.pop(index_list.index(random.choice(index_list)))
-            self.create_visual_image(image=f'img/img_{no_trial}_{j}.png',
-                                     size=self.size(f'img_{no_trial}_{j}.png')).draw()
-            self.win.flip()
-            core.wait(2)
-            self.create_visual_text("Les deux barres sont-elles parallèles ? \n\n Non / Oui").draw()
-            self.win.flip()
-            time_stamp = time.time() - exp_start_timestamp
-            resp, rt = self.get_response_with_time(self.response_pad)
-            good_ans = [self.yes_key_code if j != 4 else self.no_key_code][0]
-            if self.response_pad:
-                self.update_csv(self.participant, no_trial, j, resp, good_ans, resp == good_ans,
-                                round(rt - time_stamp, 2), round(rt, 2))
+    def example(self, exp_start_timestamp):
+        score_example = 0
+        example = self.create_visual_text(text="Commençons par un petit entraînement")
+        tutoriel_end = self.create_visual_text(
+            text="L'entraînement est désormais terminé"
+        )
+        example.draw()
+        self.create_visual_text(self.next, pos=(0, -0.4), font_size=0.04).draw()
+        self.win.flip()
+        self.wait_yes(self.response_pad)
+        for u in range(100, 103):
+            if self.task(u, exp_start_timestamp, time.time(), True):
+                score_example += 1
+                self.create_visual_text(
+                    f"Bravo ! Vous avez {score_example}/{u - 99}"
+                ).draw()
+                self.win.flip()
+                core.wait(2)
             else:
-                self.update_csv(self.participant, no_trial, j, resp, good_ans, resp == good_ans, round(rt, 2),
-                                round(time.time() - exp_start_timestamp, 2))
-            if len(index_list) == 0:
-                break
+                self.create_visual_text(
+                    f"Dommage... Vous avez {score_example}/{u - 99}"
+                ).draw()
+                self.win.flip()
+                core.wait(2)
+        self.create_visual_text(f"Vous avez obtenu {score_example}/3").draw()
+        self.win.flip()
+        core.wait(5)
+        tutoriel_end.draw()
+        self.win.flip()
+        core.wait(5)
 
 
 exp = Symetry(csv_folder="csv")
